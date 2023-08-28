b670ab2272a0191d152f66e3ebc410318e02d5ca
Thabiso Magwaza <magwazathabiso9@gmail.com>
2023-08-28 22:24:49 +0200

    finished custom select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,65 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
-const Select = ({ label, value, onChange, children }) => {
+const Wrapper = styled.div`
+  position: relative;
+
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const Presentation = styled.div`
+  color: ${COLORS.gray700};
+
+  background: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+
+  padding-right: 52px;
+
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+
+  margin-top: auto;
+  margin-bottom: auto;
+
+  width: 24px;
+  height: 24px;
+
+  pointer-events: none;
+`;
+
+const Select = ({ value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <Presentation>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id="chevron-down" size={24} strokeWidth={2} />
+        </IconWrapper>
+      </Presentation>
+    </Wrapper>
   );
 };
 
